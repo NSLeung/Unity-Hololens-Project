7af93d257d79cb75a03077f1bd7fe6f1f64909c2
Nayman Leung <naymansleung@gmail.com>
2016-08-17 16:08:59 -0400

    fixed some stuff and started on TILT SYSTEMS

--- generateAtoms.cs
+++ generateAtoms.cs
@@ -16,12 +16,12 @@ public class GenerateAtoms : MonoBehaviour
 
     private float XatomsRadius = 0.25f;
     private float BatomsRadius = 0.7f;
-    public Color XatomsColor = Color.red;
-    public Color BatomsColor = Color.blue;
-    public Color AatomsColor = Color.white;
+    private Color XatomsColor = Color.red;
+    private Color BatomsColor = Color.blue;
+    private Color AatomsColor = Color.white;
 
-    private Vector3[] XatomCoords = new Vector3[7];
-    private Vector3[] AatomCoords = new Vector3[8];
+    public Vector3[] XatomCoords = new Vector3[7];
+    public Vector3[] AatomCoords = new Vector3[8];
 
     private Vector3[][] meshVerts = new Vector3[8][];
     private Mesh mesh;/* = new Mesh();*/
@@ -42,43 +42,144 @@ public class GenerateAtoms : MonoBehaviour
     public GameObject SelectedCubeCage;
     //private int octCounter = 1;
     public Slider slider;
-    public float sliderValue;
+    private float sliderValue;
 
-    public bool rotating= true;
+    //max value is changed in the ChangeValue script
+    //public float maxSliderValue = 180;
+
+        //glazer variables
+    public string glazerTilt = "a0b0c-";
+    private string xPhase = "";
+    private string yPhase = "";
+    private string zPhase = "";
+
+    //rotation variables
+    float rotAngle = 0f;
+    Vector3 rotAxisX = new Vector3();
+    Vector3 rotAxisY = new Vector3();
+    Vector3 rotAxisZ = new Vector3();
+    Vector3 rotAxisDefault = new Vector3();
+    Quaternion rotX;
+    Quaternion rotY;
+    Quaternion rotZ;
+        //need separate rotations for each layer since the perovskite will be in phase or anti phase
+    Quaternion rotl1;
+    Quaternion rotl2;
+    Quaternion rotDef;
     //private GameObject Batom = new GameObject("B");
     void Start()
     {
-        octahedraArray = new GameObject[/*dimensions*dimensions*dimensions*/2];
-        perovskiteUnitCell = new GameObject[/*dimensions*dimensions*dimensions*/2];
+        octahedraArray = new GameObject[/*dimensions*dimensions*dimensions*/8];
+        perovskiteUnitCell = new GameObject[/*dimensions*dimensions*dimensions*/8];
         sliderValue = slider.value;
+
+        //this is the glazer notation stuff delimiting?
+        xPhase = char.ToString(glazerTilt[1]);
+        yPhase = char.ToString(glazerTilt[3]);
+        zPhase = char.ToString(glazerTilt[5]);
+
+        //initiating axes of rotation
+        rotAxisX = Vector3.right; 
+        rotAxisY = Vector3.forward;
+        rotAxisZ = Vector3.up;
+        rotAxisDefault = new Vector3(0, 0, 0);
+
+        
+        Debug.Log(glazerTilt);
+
         generatePerovskite();
     }
    void Update()
     {
-        sliderValue = slider.value;
-        //rotateAround();
-        
+        rotAngle = slider.value; //degrees of rotation (toggled by slider value)
+        rotDef = Quaternion.AngleAxis(rotAngle, rotAxisDefault);
+        //initiating Quaternion rotations (might need to be in Update function because rotations are always changing)
+        rotX = Quaternion.AngleAxis(rotAngle, rotAxisX);
+        rotY = Quaternion.AngleAxis(rotAngle, rotAxisY);
+        rotZ = Quaternion.AngleAxis(rotAngle, rotAxisZ);
+        //if y tilting is possible
+        if (!zPhase.Contains("0"))
+        {
             
-        StartCoroutine(RotateMe(Vector3.right * sliderValue));
+            octahedraArray[0].transform.rotation = rotZ;
+
+            rotZ = Quaternion.AngleAxis(rotAngle, -rotAxisZ);
+            octahedraArray[1].transform.rotation = rotZ;
+
+            rotZ = Quaternion.AngleAxis(rotAngle, -rotAxisZ);
+            octahedraArray[2].transform.rotation = rotZ;
+
+            rotZ = Quaternion.AngleAxis(rotAngle, rotAxisZ);
+            octahedraArray[3].transform.rotation = rotZ;
+
+            if (zPhase.Contains("+"))
+            {
+                octahedraArray[4].transform.rotation = rotZ;
+
+                rotZ = Quaternion.AngleAxis(rotAngle, -rotAxisZ);
+                octahedraArray[5].transform.rotation = rotZ;
+
+                rotZ = Quaternion.AngleAxis(rotAngle, -rotAxisZ);
+                octahedraArray[6].transform.rotation = rotZ;
+
+                rotZ = Quaternion.AngleAxis(rotAngle, rotAxisZ);
+                octahedraArray[7].transform.rotation = rotZ;
+            }
+            else if (zPhase.Contains("-"))
+            {
+                rotZ = Quaternion.AngleAxis(rotAngle, -rotAxisZ);
+                octahedraArray[4].transform.rotation = rotZ;
+
+                rotZ = Quaternion.AngleAxis(rotAngle, rotAxisZ);
+                octahedraArray[5].transform.rotation = rotZ;
+
+                rotZ = Quaternion.AngleAxis(rotAngle, rotAxisZ);
+                octahedraArray[6].transform.rotation = rotZ;
+
+                rotZ = Quaternion.AngleAxis(rotAngle, -rotAxisZ);
+                octahedraArray[7].transform.rotation = rotZ;
+            }
+        }
+            
         
-    }
-    //octahedraArray[0].transform.RotateAround(gameObject.transform.FindChild(perovskiteUnitCell[0].name).FindChild(octahedraArray[0].name).FindChild("B").position, Vector3.up, sliderValue);
-    //THIS IS THE COROUTINE FOR ROTATING???!
-    IEnumerator RotateMe(Vector3 byAngles)
+        
+        
+        //rotAll = rotX * rotY;
+        ///rotAll *= rotZ;
+        /*
+        //bottom layer (consider using if statement if dimensions get bigger?)
+        octahedraArray[0].transform.rotation = rotl1;
+        octahedraArray[1].transform.rotation = rotl1;
+        octahedraArray[2].transform.rotation = rotl1;
+        octahedraArray[3].transform.rotation = rotl1;
+        */
+
+
+    }/*
+   //Coroutine for rotating
+    IEnumerator RotateMe(GameObject obj, Vector3 byAngles)
     {
-        var fromAngle = transform.rotation;
-        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
         
-            octahedraArray[1].transform.rotation = Quaternion.Lerp(fromAngle, toAngle, 1f);
+        var fromAngle = obj.transform.rotation;
+        
+        var toAngle = Quaternion.Euler(byAngles);
+        
+            obj.transform.rotation = Quaternion.Lerp(fromAngle, toAngle, 1f);
             yield return null;
         
     }
-
-    public void resetTransforms()
+    */
+    public void resetX()
     {
-        transformX = 0;
-        transformY = 0;
-        transformZ = 0;
+        transformX -=2;
+        }
+    public void resetY()
+    {
+        transformY -= 2;
+    }
+    public void resetZ()
+    {
+        transformZ -= 2;
     }
     public void generatePerovskite()
     {
@@ -112,19 +213,25 @@ public class GenerateAtoms : MonoBehaviour
             
             octahedraArray[m] = new GameObject("Octahedra " + (m));
             perovskiteUnitCell[m] = new GameObject("Perovskite Unit Cell " + (m));
-
+            
             for (int i = 0; i < AatomsArray.Length; i++)
             {
+
                 //Debug.Log("X: "+transformX);
                 //Debug.Log("Z: " + transformZ);
-                AatomCoords[i] += new Vector3(transformX, transformY, transformZ);
-
-
+                //AatomCoords[i] += new Vector3(transformX, transformY, transformZ);
+                if (m == 2)
+                {
+                    //Debug.Log(AatomCoords[i]);
+                }
+                
+               // Debug.Log(AatomCoords[i]);
                 //make the actual sphere
                 AatomsArray[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 AatomsArray[i].name = "A " + (i + 1);
 
                 AatomsArray[i].transform.position = AatomCoords[i];
+                
                 //radius
                 AatomsArray[i].transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
@@ -133,44 +240,63 @@ public class GenerateAtoms : MonoBehaviour
                 AatomsArray[i].transform.parent = perovskiteUnitCell[m].transform;
                 AatomsArray[i].GetComponent<Renderer>().material.color = AatomsColor;
 
+
+                
+                
                 //add interactible script
                 AatomsArray[i].AddComponent<Interactible>();
-                if (i < XatomsArray.Length)
-                {
-                    XatomCoords[i] += new Vector3(transformX, transformY, transformZ);
+
+                
+                    if (i < XatomsArray.Length)
+                    {
+
+                        //XatomCoords[i] += new Vector3(transformX, transformY, transformZ);
 
 
-                    //make the actual sphere
-                    XatomsArray[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    XatomsArray[i].name = "X " + (i + 1);
-                    XatomsArray[i].transform.position = XatomCoords[i];
-                    //radius
-                    XatomsArray[i].transform.localScale = new Vector3(XatomsRadius, XatomsRadius, XatomsRadius);
+                        //make the actual sphere
+                        XatomsArray[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        XatomsArray[i].name = "X " + (i + 1);
+                        XatomsArray[i].transform.position = XatomCoords[i];
+                        //radius
+                        XatomsArray[i].transform.localScale = new Vector3(XatomsRadius, XatomsRadius, XatomsRadius);
 
-                    //make octahedra the parent of the X atoms
-                    XatomsArray[i].transform.parent = octahedraArray[m].transform;
-                    octahedraArray[m].transform.parent = perovskiteUnitCell[m].transform;
-                    perovskiteUnitCell[m].transform.parent = gameObject.transform;
+                        //make octahedra the parent of the X atoms
+                        XatomsArray[i].transform.parent = octahedraArray[m].transform;
 
-                    XatomsArray[i].GetComponent<Renderer>().material.color = XatomsColor;
+                        octahedraArray[m].transform.parent = perovskiteUnitCell[m].transform;
+                        perovskiteUnitCell[m].transform.parent = gameObject.transform;
 
-                    /*
-                    //COLLIDERS??!?
-                    //XatomsArray[i].AddComponent<Collider>();
-                    XatomsArray[i].GetComponent<Collider>().enabled = false;
-                    XatomsArray[i].AddComponent<Rigidbody>();
-                    XatomsArray[i].GetComponent<Rigidbody>().useGravity = false;
-                    */
-                    //add interactible script
-                    XatomsArray[i].AddComponent<Interactible>();
-                }
+                        XatomsArray[i].GetComponent<Renderer>().material.color = XatomsColor;
 
+                        /*
+                        //COLLIDERS??!?
+                        //XatomsArray[i].AddComponent<Collider>();
+                        XatomsArray[i].GetComponent<Collider>().enabled = false;
+                        XatomsArray[i].AddComponent<Rigidbody>();
+                        XatomsArray[i].GetComponent<Rigidbody>().useGravity = false;
+                        */
+                        //add interactible script
+                        XatomsArray[i].AddComponent<Interactible>();
+                    }
+                
             }
             //resetTransforms();
             //configure settings for B atom (center)
             XatomsArray[4].name = "B";
             XatomsArray[4].transform.localScale = new Vector3(BatomsRadius, BatomsRadius, BatomsRadius);
             XatomsArray[4].GetComponent<Renderer>().material.color = BatomsColor;
+            //octahedraArray[m].transform.position = XatomCoords[4];
+
+            //og line of code below
+            //octahedraArray[m].transform.position = /*XatomsArray[4].transform.position*/ new Vector3(transformX, transformY, transformZ);
+            perovskiteUnitCell[m].transform.position = /*XatomsArray[4].transform.position*/ new Vector3(transformX, transformY, transformZ);
+            if (m == 2)
+            {
+                //octahedraArray[m].transform.position = new Vector3(0, 0, 2);
+            }
+            //Debug.Log(octahedraArray[m].transform.position);
+
+            
 
             meshVerts = new Vector3[][]{
                 new Vector3[3] { XatomCoords[0], XatomCoords[2], XatomCoords[3] },
@@ -183,6 +309,16 @@ public class GenerateAtoms : MonoBehaviour
                 new Vector3[3] { XatomCoords[0], XatomCoords[3], XatomCoords[5] },
 
             };
+            
+            //test for xatomsarray
+            for (int p = 0; p < meshVerts.Length; p++)
+            {
+                for (int o = 0; o < meshVerts[0].Length; o++)
+                {
+                    //transform the octahedral planes
+                    meshVerts[p][o] += new Vector3(transformX, transformY, transformZ);
+                }
+            }
             /*
             planeArray[0].AddComponent<MeshFilter>();
             planeArray[0].AddComponent<MeshRenderer>();
@@ -228,73 +364,40 @@ public class GenerateAtoms : MonoBehaviour
             ///HERE BEGINS THE TRANSFORMING
             switch (m%7)
             {
-                case 0:
-                    ////Debug.Log("WEF WEF EW");
-                //x+=2
-                transformX += 2;
-                    
-                ////generateOctahedra();
-                //resetTransforms();
+                case 0:                    
+                    transformX +=2;
                 break;
 
                 case 1:
                     //z+=2
                     
-                    resetTransforms();
-                    transformX -= 2;
-                    transformZ += 2;
-                //generateOctahedra();
-                //resetTransforms();
+                    resetX();
+                    transformZ = 2;
                     break;
                 case 2:
-                    //x, z+=2
-                    resetTransforms();
-                    //transformZ -= 2;
-                    transformX += 2;
-                    //keep the transformZ from last case (1)
-                //transformZ += 2;
-                //generateOctahedra();
-                //resetTransforms();
+                    transformX = 2;
                     break;
                 case 3:
                     //y+=2
-                    resetTransforms();
-                    transformX -= 2;
-                    transformZ -= 2;
-                transformY += 2;
-                //generateOctahedra();
-                //resetTransforms();
+                    resetX();
+                    resetZ();
+                    transformY += 2;
                     break;
 
                 case 4:
 
                     //y+=2, x+=2
-                    resetTransforms();
-                //transformY += 2;
                 transformX += 2;
-                //generateOctahedra();
-                //resetTransforms();
                     break;
                 case 5:
-                //y+=2, z+=2
-                //transformY += 2;
-                
-                    resetTransforms();
-                    transformX -= 2;
+                    //y+=2, z+=2
+                    resetX();
                     transformZ += 2;
-                    //generateOctahedra();
-                    //resetTransforms();
                     break;
                 case 6:
 
                     //x, z+=2, y+=2
-                    //transformY += 2;
-                    resetTransforms();
                 transformX += 2;
-                    //resetTransforms();
-                //transformZ += 2;
-                //generateOctahedra();
-                //resetTransforms();
                     break;
                 //default: break;
         }
